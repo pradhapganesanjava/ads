a79c503c9369c0df15d04964b59f953d78c01dc2
pganesan <pganesan@linkedin.biz>
2024-11-10 18:55:18 -0800

    refactor

---
+++ js/const.js
@@ -0,0 +1,9 @@
+// js/const.js
+
+const RANGE = 'fb_all_prob'; // Adjust the range as per your Google Sheet
+const CLIENT_ID = '1001531064954-1207m61i9hkrausk96fe812jvqvjv4ef.apps.googleusercontent.com';
+const SPREADSHEET_ID = '1MgmXRQw-hdMkJDDuE5XBswAz9TlO0NEPmcdVgyGDY0c'
+const REDIRECT_URI = `https://pradhapganesanjava.github.io/pg-ads/pg_ads.html`;  
+
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
