27a2d8cf5bdbb00ef3ae8c9001909bdeafbfb80f
pganesan <pganesan@linkedin.biz>
2024-11-10 19:13:38 -0800

    refactor

--- js/const.js
+++ js/const.js
@@ -1,9 +1,9 @@
 // js/const.js
 
-const RANGE = 'fb_all_prob'; // Adjust the range as per your Google Sheet
-const CLIENT_ID = '1001531064954-1207m61i9hkrausk96fe812jvqvjv4ef.apps.googleusercontent.com';
-const SPREADSHEET_ID = '1MgmXRQw-hdMkJDDuE5XBswAz9TlO0NEPmcdVgyGDY0c'
-const REDIRECT_URI = `https://pradhapganesanjava.github.io/pg-ads/pg_ads.html`;  
+export const RANGE = 'fb_all_prob'; // Adjust the range as per your Google Sheet
+export const CLIENT_ID = '1001531064954-1207m61i9hkrausk96fe812jvqvjv4ef.apps.googleusercontent.com';
+export const SPREADSHEET_ID = '1MgmXRQw-hdMkJDDuE5XBswAz9TlO0NEPmcdVgyGDY0c'
+export const REDIRECT_URI = `https://pradhapganesanjava.github.io/pg-ads/pg_ads.html`;  
 
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+export const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+export const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
