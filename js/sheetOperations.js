839859aefa153ab0ca19f92ea0917a67643a5bef
pganesan <pganesan@linkedin.biz>
2024-11-11 13:09:25 -0800

    refactor

--- js/sheetOperations.js
+++ js/sheetOperations.js
@@ -19,3 +19,26 @@ export async function fetchSheetData() {
         throw err;
     }
 }
+
+export async function listMajors() {
+    let response;
+    try {
+        const response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: SPREADSHEET_ID,
+            range: RANGE,
+        });
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('content').innerText = 'No values found.';
+        return;
+    }
+    // Flatten to string to display
+    const output = range.values.reduce(
+        (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+        'Name, Major:\n');
+    document.getElementById('content').innerText = output;
+}
