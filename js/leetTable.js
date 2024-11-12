08a9a17b00a1e9751c41849197cc22df6161e232
pganesan <pganesan@linkedin.biz>
2024-11-11 21:36:10 -0800

    fix

--- js/leetTable.js
+++ js/leetTable.js
@@ -31,27 +31,55 @@ export function renderTable(data) {
         $('#sheetDataTable').DataTable().destroy();
     }
 
+    // $('#sheetDataTable').DataTable({
+    //     data: tableData,
+    //     columns: FILTER_HEADERS.map(header => ({ title: header })),
+    //     pageLength: 25,
+    //     lengthMenu: [[25, 50, 100, -1], [25, 50, 100, "All"]],
+    //     responsive: true,
+    //     // scrollY: '50vh', // Adjust this value as needed
+    //     scrollY: '100%', // Use percentage instead of vh units
+    //     scrollCollapse: true,
+    //     paging: true,
+    //     dom: '<"row"<"col-sm-12 col-md-6"B><"col-sm-12 col-md-6"f>>rt<"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
+    //     buttons: [
+    //         'copy', 'csv', 'excel', 'pdf', 'print'
+    //     ]
+    // });
+
     $('#sheetDataTable').DataTable({
         data: tableData,
         columns: FILTER_HEADERS.map(header => ({ title: header })),
         pageLength: 25,
         lengthMenu: [[25, 50, 100, -1], [25, 50, 100, "All"]],
         responsive: true,
-        // scrollY: '50vh', // Adjust this value as needed
-        scrollY: '100%', // Use percentage instead of vh units
+        scrollY: true,
         scrollCollapse: true,
         paging: true,
-        dom: '<"row"<"col-sm-12 col-md-6"B><"col-sm-12 col-md-6"f>>rt<"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
+        dom: '<"row"<"col-sm-12 col-md-6"B><"col-sm-12 col-md-6"f>><"row"<"col-sm-12"tr>><"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
+        ],
+        drawCallback: function (settings) {
+            var api = this.api();
+            var $table = $(api.table().node());
+
+            // Adjust the height of the scrollBody
+            var scrollBody = $table.closest('.dataTables_scrollBody');
+            var wrapper = $table.closest('.dataTables_wrapper');
+            var availableHeight = wrapper.height() - (wrapper.find('.dataTables_filter').outerHeight(true) + wrapper.find('.dataTables_info').outerHeight(true) + wrapper.find('.dataTables_paginate').outerHeight(true));
+            scrollBody.css('max-height', availableHeight + 'px');
+
+            // Adjust columns and redraw
+            api.columns.adjust().draw();
+        }
     });
 }
 
-export function initializeDataTable() {
-    $('#sheetDataTable').DataTable({
-        responsive: true,
-        pageLength: 25,
-        lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]]
-    });
-}
+// export function initializeDataTable() {
+//     $('#sheetDataTable').DataTable({
+//         responsive: true,
+//         pageLength: 25,
+//         lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]]
+//     });
+// }
